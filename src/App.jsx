2e42c5f940a110a0b1cd30b73920d368b1e15a5d
Rijal Islami <rijalislamir@gmail.com>
2022-07-07 23:57:27 +0700

    Install tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Numpad from "./components/Numpad";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello Worlds!</h1>
+    </div>
+  );
+}
+
+export default App;
